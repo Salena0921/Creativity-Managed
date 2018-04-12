c23149393bdeeb2c9b060ab671e7b2ce93ca08b2
Karissa McKendall <kmckendall@gmail.com>
2018-04-12 19:19:51 -0400

    updated

--- creativitymanaged/models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	User:require("./User.js"),
-	Pattern: require("./patterns"),
-    Fabric: require("./fabrics"),
-    Design: require("./designs")
-}
-//add more models as you create them
