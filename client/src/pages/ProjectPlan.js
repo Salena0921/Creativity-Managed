8be7fa0d6cd0aa4962360040c6ea2f9ab7678183
Karissa McKendall <kmckendall@gmail.com>
2018-04-16 14:42:51 -0400

    added a Project Plan page

---
+++ client/src/pages/ProjectPlan.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+import Card from "../components/Card";
+import Nav from "../components/Nav";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+import API from "../utils/API";
+import '../App.css';
+
+class ProjectPlan extends Component {
+
+}
+export default ProjectPlan;
